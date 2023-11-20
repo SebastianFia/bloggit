10e07f04545187a6c75c483f962007b9236399d4
seba42 <fiasebastian.04@gmail.com>
2023-11-20 07:38:46 +0100

    server: added routes mod

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,14 @@
-use actix_web::{App, HttpServer, web, Responder};
+use actix_web::{web, App, HttpServer};
+use bloggit::routes::home;
 use std::net::TcpListener;
-
-async fn home() -> impl Responder {
-    "hello from home"
-}
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("localhost:8000")?;
-    HttpServer::new(|| 
-        App::new()
-            .route("/", web::get().to(home))
-    )
-    .listen(listener)?
-    .run()
-    .await?;
+    HttpServer::new(|| App::new().route("/", web::get().to(home)))
+        .listen(listener)?
+        .run()
+        .await?;
 
     Ok(())
 }
