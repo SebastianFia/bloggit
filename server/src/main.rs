27c8babef8518b64a71254f67b0a7dbc88dc42b2
seba42 <fiasebastian.04@gmail.com>
2023-11-20 07:30:25 +0100

    basic actix web server structure

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("hello world!");
+use actix_web::{App, HttpServer, web, Responder};
+use std::net::TcpListener;
+
+async fn home() -> impl Responder {
+    "hello from home"
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("localhost:8000")?;
+    HttpServer::new(|| 
+        App::new()
+            .route("/", web::get().to(home))
+    )
+    .listen(listener)?
+    .run()
+    .await?;
+
+    Ok(())
 }
